cc1266fb94b549ed704fef240438aeb8bdca2d71
Adam <adamg110911@gmail.com>
2024-02-15 20:07:48 -0800

    Add round results display

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-// const rockBtn = document.querySelector(".rock-btn");
-// const paperBtn = document.querySelector(".paper-btn");
-// const scissorsBtn = document.querySelector(".scissors-btn");
 const currentRound = document.querySelector("#currentRound");
 const yourScore = document.querySelector(".yourScore");
 const opponentScore = document.querySelector(".opponentScore");
 const gameOptionBtns = document.querySelectorAll(".game-btn");
-let round = 0;
+const gameOutput = document.querySelector(".game-output");
+const roundResults = document.querySelector(".round-results");
+let round = 1;
 let playerScore = 0;
 let computerScore = 0;
 
+currentRound.innerText = round;
+yourScore.innerText = playerScore;
+opponentScore.innerText = computerScore;
+
 gameOptionBtns.forEach(btn => {
   btn.addEventListener('click', playGame);
-})
+});
 
 function getComputerChoice() {
   const optionsArr = ["Rock", "Paper", "Scissors"];
@@ -39,19 +42,33 @@ function playRound(playerOption, computerOption) {
   } else if (playerOption === "scissors" && computerOption === "paper") {
     playerScore++;
     return "You Win!"
-  } else if (playerOption === computerOption){
+  } else if (playerOption === computerOption) {
     return "It's a draw!";
   }
 }
 
 function playGame(e) {
+  e.stopPropagation();
+  let playerOption = e.target.dataset.option.toLowerCase();
+  let computerOption = getComputerChoice().toLowerCase();
+  let result = playRound(playerOption, computerOption);
   if (round < 5) {
-    let playerOption = e.target.dataset.option.toLowerCase();
-    let computerOption = getComputerChoice().toLowerCase();
-    let result = playRound(playerOption, computerOption);
+    // update player or opponent score
     round++;
+    yourScore.innerText = playerScore;
+    opponentScore.innerText = computerScore;
     currentRound.innerText = round;
-    console.log(playerScore, computerScore);
   } else if (round === 5) {
+    currentRound.innerText = 5;
+    // update player or opponent score
+    yourScore.innerText = playerScore;
+    opponentScore.innerText = computerScore;
+    if (playerScore > computerScore) {
+      gameOutput.appendChild(document.createTextNode("You win the game!"))
+    } else if (playerScore === computerScore) {
+      gameOutput.appendChild(document.createTextNode("It's a draw for this game."))
+    } else {
+      gameOutput.appendChild(document.createTextNode("Opponent wins this time."))
+    }
   }
 }
